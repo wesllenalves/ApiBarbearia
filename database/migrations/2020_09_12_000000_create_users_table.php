bd6a4097b1784d4738e71de00f747885b025f604
wesllenalves <wesllenalves@gmail.com>
2020-09-28 15:22:02 -0300

    atualizando tabela de usuario

--- database/migrations/2020_09_12_000000_create_users_table.php
+++ database/migrations/2020_09_12_000000_create_users_table.php
@@ -19,8 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('avatar');
-            $table->string('stars');
+            $table->string('avatar')->nullable();
+            $table->string('stars')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
